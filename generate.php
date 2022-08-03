74eeb8704a77f8ded48e479e2f1375d9261ccdad
Krystian Podemski <podemski.krystian@gmail.com>
2022-08-03 19:36:36 +0200

    Minor improvements

--- generate.php
+++ generate.php
@@ -4,6 +4,8 @@ if ($argc < 2) {
     die('Please pass a GitHub token as argument');
 }
 
+$timeStart = microtime(true);
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new \Github\Client();
@@ -27,33 +29,90 @@ function getModules($client): array
     return $modules;
 }
 
+function getClassByNbCommitsAhead(int $nbCommitsAhead): string
+{
+    switch ($nbCommitsAhead) {
+        case ($nbCommitsAhead > 0 && $nbCommitsAhead <= 25):
+            $trClass = 'light';
+            break;
+        case ($nbCommitsAhead > 25 && $nbCommitsAhead <= 100):
+            $trClass = 'warning';
+            break;
+        case ($nbCommitsAhead > 100):
+            $trClass = 'danger';
+            break;
+        default:
+            $trClass = 'default';
+            break;
+    }
+
+    return 'table-' . $trClass;
+}
+
 $modulesToProcess = getModules($client);
 $template = file_get_contents(__DIR__.'/src/template.tpl');
 
 $tableRows = [];
 $i = 1;
 
+
 foreach ($modulesToProcess as $moduleToProcess) {
     $repositoryName = $moduleToProcess;
     $data = $branchManager->getReleaseData($repositoryName);
+    $nbCommitsAhead = $data['ahead'];
 
-    if ($data['ahead'] == 0) {
-        $tableRows[] = '<tr>
+    $trClass = getClassByNbCommitsAhead($nbCommitsAhead);
+
+    if ($nbCommitsAhead == 0) {
+        $tableRows[] = [
+            'html' => '<tr class="table-success">
               <th scope="row">'.$i.'</th>
               <td><a href="https://github.com/prestashop/'.$repositoryName.'">'.$repositoryName.'</a></td>
               <td>NO</td>
               <td>0</td>
-            </tr>';
+            </tr>',
+            'ahead' => 0,
+        ];
     } else {
-        $tableRows[] = '<tr>
+        $tableRows[] = [
+            'html' =>'<tr class="'.$trClass.'">
               <th scope="row">'.$i.'</th>
               <td><a href="https://github.com/prestashop/'.$repositoryName.'">'.$repositoryName.'</a></td>
               <td>YES</td>
               <td>'.$data['ahead'].'</td>
-            </tr>';
+            </tr>',
+            'ahead' => $data['ahead'],
+        ];
     }
+
+    uasort($tableRows, function ($a, $b) {
+        if ($a['ahead'] == $b['ahead']) {
+            return 0;
+        }
+
+        return ($a['ahead'] > $b['ahead']) ? -1 : 1;
+    });
+
+    $tableContent = array_map(function ($row) {
+        return $row['html'];
+    }, $tableRows);
 
     $i++;
 }
 
-file_put_contents(__DIR__.'/docs/index.html', str_replace('{%%placeholder%%}', implode('', $tableRows), $template));
+file_put_contents(
+    __DIR__ . '/docs/index.html',
+    str_replace(
+        [
+            '{%%placeholder%%}',
+            '{%%latestUpdateDate%%}',
+        ],
+        [
+            implode('', $tableContent),
+            date('l, j F Y H:i'),
+        ],
+        $template
+    )
+);
+
+die('Generated in ' . (microtime(true) - $timeStart) . ' seconds');
