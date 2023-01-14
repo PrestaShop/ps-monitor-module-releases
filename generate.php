4c9fc5f5d825e0afa2a2d5b885c53c96dae5c88a
Hlavtox <daniel.hlavacek@hotmail.cz>
2023-01-14 14:04:45 +0100

    Improve release monitor

--- generate.php
+++ generate.php
@@ -1,16 +1,26 @@
 <?php
 
-if ($argc < 2) {
-    die('Please pass a GitHub token as argument');
-}
-
 $timeStart = microtime(true);
-
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Get token from parameters, CLI or GET
+$token = null;
+if (isset($argc) && !empty($argv[1])) {
+    echo 'Using CLI token</br>';
+    $token = $argv[1];
+}
+if (!empty($_GET['token'])) {
+    echo 'Using GET token</br>';
+    $token = $_GET['token'];
+}
+if (empty($token)) {
+    die('Please pass a GitHub token as argument in the CLI, or using "token" get parameter.');
+}
+
+// Initialize github client and login
 $client = new \Github\Client();
-$token = $argv[1];
 $client->authenticate($token, null, Github\Client::AUTH_HTTP_TOKEN);
+echo 'Login successful</br>';
 
 $branchManager = new \App\PrestaShopModulesReleaseMonitor\BranchManager($client);
 
@@ -32,6 +42,9 @@ function getModules($client): array
 function getClassByNbCommitsAhead(int $nbCommitsAhead): string
 {
     switch ($nbCommitsAhead) {
+        case ($nbCommitsAhead == 0):
+            $trClass = 'success';
+            break;
         case ($nbCommitsAhead > 0 && $nbCommitsAhead <= 25):
             $trClass = 'light';
             break;
@@ -49,52 +62,110 @@ function getClassByNbCommitsAhead(int $nbCommitsAhead): string
     return 'table-' . $trClass;
 }
 
+echo 'Fetching list of modules</br>';
 $modulesToProcess = getModules($client);
-$template = file_get_contents(__DIR__.'/src/template.tpl');
+// $modulesToProcess = ['blockreassurance', 'statsstock'];
+echo 'Module list initialized, found ' . count($modulesToProcess) . ' module</br>';
+echo 'Rendering table</br>';
 
+// Data we will use to render an overview
+$notifications = [
+    'not_published' => [],
+    'unclosed_milestone' => [],
+    'no_next_milestone' => [],
+    'old_milestones' => [],
+];
+
+$template = file_get_contents(__DIR__.'/src/template.tpl');
 $tableRows = [];
 $i = 1;
 
+// Let's go through all prestashop modules and process data we need
+foreach ($modulesToProcess as $moduleName) {
 
-foreach ($modulesToProcess as $moduleToProcess) {
-    $repositoryName = $moduleToProcess;
-    $data = $branchManager->getReleaseData($repositoryName);
-    $nbCommitsAhead = $data['ahead'];
+    // Get data about repository
+    $data = $branchManager->getReleaseData($moduleName);
 
-    $trClass = getClassByNbCommitsAhead($nbCommitsAhead);
-
-    $link = $assignee = '';
-
-    if ($data['pullRequest']) {
-        $link = '<a href="'.$data['pullRequest']['link'].'">#PR' . $data['pullRequest']['number'] . '</a>';
-        $assignee = $data['pullRequest']['assignee'];
-    }
-
-    if ($nbCommitsAhead == 0) {
-        $tableRows[] = [
-            'html' => '<tr class="table-success">
-              <th scope="row">'.$i.'</th>
-              <td><a href="https://github.com/prestashop/'.$repositoryName.'">'.$repositoryName.'</a></td>
-              <td>NO</td>
-              <td>0</td>
-              <td>' . $data['releaseDate'] . '</td>
-              <td>' . $link . ' ' . $assignee . '</td>
-            </tr>',
-            'ahead' => 0,
-        ];
+    // Process info about last release
+    $lastReleaseInformation = [];
+    if (!empty($data['latestRelease'])) {
+        $lastReleaseInformation[] = 'Name: <a href="' . $data['latestRelease']['url'] . '">' . $data['latestRelease']['name'] . '</a>';
+        $lastReleaseInformation[] = 'Tag: ' . $data['latestRelease']['tag'];
+        if (empty($data['latestRelease']['date_published'])) {
+            $lastReleaseInformation[] = '<strong style="color:red;">Release not published yet!</strong>';
+            $notifications['not_published'][] = $moduleName;
+        }
     } else {
-        $tableRows[] = [
-            'html' =>'<tr class="'.$trClass.'">
-              <th scope="row">'.$i.'</th>
-              <td><a href="https://github.com/prestashop/'.$repositoryName.'">'.$repositoryName.'</a></td>
-              <td>YES</td>
-              <td>' . $data['ahead'] . '</td>
-              <td>' . $data['releaseDate'] . '</td>
-              <td>' . $link . ' ' . $assignee . '</td>
-            </tr>',
-            'ahead' => $data['ahead'],
-        ];
+        $lastReleaseInformation[] = 'No release yet';
     }
+
+    // Add info about milestones to last release
+    $lastMilestone = '';
+    if (!empty($data['milestoneInformation']['last'])) {
+        $lastMilestone = 'Milestone: <a href="' . $data['milestoneInformation']['last'][0]['url'] . '">
+        ' . $data['milestoneInformation']['last'][0]['title'] . '</a>';
+        if ($data['milestoneInformation']['last'][0]['state'] == 'open') {
+            $lastMilestone .= ', <strong style="color:red;">NOT CLOSED!</strong>';
+            $notifications['unclosed_milestone'][] = $moduleName;
+        }
+    } else {
+        $lastMilestone = 'Not found';
+    }
+    $lastReleaseInformation[] = $lastMilestone;
+
+    // Last release date
+    $lastReleaseDate = (!empty($data['latestRelease']) ? $data['latestRelease']['date'] : 'NA');
+
+    // Needs release?
+    $needsRelease = ($data['ahead'] > 0 ? 'YES' : 'NO');
+
+    // Next release information
+    $nextReleaseInformation = [];
+    if (!empty($data['pullRequest'])) {
+        $nextReleaseInformation[] = 
+            'PR: <a href="'. $data['pullRequest']['link'] . '">
+            #' . $data['pullRequest']['number'] . ' - ' . $data['pullRequest']['title'] . 
+            '</a>';
+        if ($data['pullRequest']['waitingForQa']) {
+            $nextReleaseInformation[] = '<strong style="color:green;">Status: Waiting for QA</strong>';
+        } else {
+            $nextReleaseInformation[] = '<strong style="color:red;">Status: Needs action</strong>';
+        }
+        if (!empty($data['pullRequest']['assignee'])) {
+            $nextReleaseInformation[] = 'Assignee: ' . $data['pullRequest']['assignee'];
+        }
+    }
+
+    // Add info about next milestones
+    if (!empty($data['milestoneInformation']['next'])) {
+        $tmp = [];
+        foreach ($data['milestoneInformation']['next'] as $m) {
+            $tmp[] = '<a href="' . $m['url'] . '">' . $m['title'] . '</a>';
+        }
+        $nextReleaseInformation[] = 'Milestone(s): ' . implode(", ", $tmp);
+    } else {
+        $nextReleaseInformation[] = '<strong style="color:red;">Milestone not found!</strong>';
+        $notifications['no_next_milestone'][] = $moduleName;
+    }
+
+    // Info about old milestones
+    if (!empty($data['milestoneInformation']['old'])) {
+        $notifications['old_milestones'][] = $moduleName;
+    }
+
+    // Render the table row
+    $tableRows[] = [
+        'html' => '<tr class="' . getClassByNbCommitsAhead($data['ahead']) . '">
+            <th scope="row">'.$i.'</th>
+            <td><a href="https://github.com/prestashop/' . $moduleName . '">' . $moduleName . '</a></td>
+            <td>' . $needsRelease . '</td>
+            <td>' . $data['ahead'] . '</td>
+            <td>' . $lastReleaseDate . '</td>
+            <td>' . implode('<br/>', $lastReleaseInformation) . '</td>
+            <td>' . implode('<br/>', $nextReleaseInformation) . '</td>
+        </tr>',
+        'ahead' => $data['ahead'],
+    ];
 
     uasort($tableRows, function ($a, $b) {
         if ($a['ahead'] == $b['ahead']) {
@@ -111,19 +182,47 @@ foreach ($modulesToProcess as $moduleToProcess) {
     $i++;
 }
 
+// Process notifications
+$notifications_html = '';
+if (!empty($notifications['not_published'])) {
+    $notifications_html .= '<div class="alert alert-warning" role="alert">
+        Modules <strong>' . implode(', ', $notifications['not_published']) . '</strong> have releases created, but not published.
+    </div>';
+}
+if (!empty($notifications['unclosed_milestone'])) {
+    $notifications_html .= '<div class="alert alert-warning" role="alert">
+        Modules <strong>' . implode(', ', $notifications['unclosed_milestone']) . '</strong> have unclosed milestones, close them.
+    </div>';
+}
+if (!empty($notifications['no_next_milestone'])) {
+    $notifications_html .= '<div class="alert alert-warning" role="alert">
+        Modules <strong>' . implode(', ', $notifications['no_next_milestone']) . '</strong> don\'t have next milestones.
+    </div>';
+}
+if (!empty($notifications['old_milestones'])) {
+    $notifications_html .= '<div class="alert alert-warning" role="alert">
+        Modules <strong>' . implode(', ', $notifications['old_milestones']) . '</strong> have some old unclosed milestones.
+    </div>';
+}
+
+echo 'Rendering finished, saving to <a href="docs/index.html">index.html</a></br>';
+
 file_put_contents(
     __DIR__ . '/docs/index.html',
     str_replace(
         [
-            '{%%placeholder%%}',
+            '{%%tableBody%%}',
             '{%%latestUpdateDate%%}',
+            '{%%notifications%%}',
         ],
         [
             implode('', $tableContent),
             date('l, j F Y H:i'),
+            $notifications_html,
         ],
         $template
     )
 );
 
-die('Generated in ' . (microtime(true) - $timeStart) . ' seconds');
+echo 'Generated in ' . (microtime(true) - $timeStart) . ' seconds</br>';
+die();
